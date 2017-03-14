be6cfb2470ccfd6cc6089e6dbe64bc0839ed1a2d
Lena900925 <rinyu.annemarie.hd@gmail.com>
2017-03-14 13:32:58 +0100

    This version works without localStorage but can create new boards and displays them with the correct title which was given in the input

--- static/storage.js
+++ static/storage.js
@@ -5,38 +5,39 @@ $(document).ready(function () {
     var boardList = [];
     function Board(title) {
         this.title = title;
-        this.boardCardList = []
+        //this.boardCardList = []
 };
 
 $("#add_new_board").click(function () {
-    var board_title = prompt("Board Title: ")
-    $(".row").append("<div class='col-sm-3'style='height: 300px; border: solid black;'><h1></h1></div>");
+    var board_title = prompt("Board Title: ");
     var board = new Board(board_title);
-    boardList.push(board)
-    localStorage.setItem("boardList", JSON.stringify(boardList));
-    $("h1:last").html(board_title);
-})
+    boardList.push(board);
+    create_division(board.title);
+});
 
 var list_all_board = function (list_of_boards) {
     for ( var oneBoard in list_of_boards) {
-        $(".row").append("<div class='col-sm-3'style='height: 300px; border: solid black;'><h1></h1></div>");
-        var board_title = function(){
-            for (key in oneBoard) {
-                alert("salala")
-                }
-            };
-        };
-        $("h1:last").html(board_title);
+        create_division(list_of_boards[oneBoard].title);
+        }
     };
 
+var create_division = function (item) {
+    $(".row").append("<div class='col-sm-3'style='height: 300px; border: solid black;'><h1></h1></div>");
+        $("h1:last").html(item);
+}
 var board1 = new Board("board1");
 var board2 = new Board("board2");
-alert(board1.title);
 boardList.push(board1);
 boardList.push(board2);
-//localStorage.setItem("boardList", JSON.stringify(boardList));
-//var board_list = JSON.parse(localStorage.getItem("boardList"));
-list_all_board(boardList);
+saveList(boardList);
+var saveList = function (List) {
+    localStorage.setItem("boardList", JSON.stringify(List))};
+
+var getList = function (List) {
+    JSON.parse(localStorage.getItem("List"));
+};
+var my_list = getList(boardList);
+alert(my_list);
 
 
 
