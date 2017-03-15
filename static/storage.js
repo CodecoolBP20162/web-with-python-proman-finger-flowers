fc341cccee3433c304ef2c09250a8a605aa6e54e
David Szilard <szilard.david91@gmail.com>
2017-03-15 12:55:05 +0100

    cards page title is board's title

--- static/storage.js
+++ static/storage.js
@@ -2,12 +2,13 @@ $(document).ready(function () {
     var boardList = [];
     function Board(title) {
         this.title = title;
+        this.cardlist = [];
     };
 
     // LIST BOARDS
     var list_all_board = function (list_of_boards) {
         for (var oneBoard in list_of_boards) {
-            create_division(list_of_boards[oneBoard].title);
+            create_division(list_of_boards[oneBoard]);
         }
     };
 
@@ -18,15 +19,28 @@ $(document).ready(function () {
         boardList.push(board);
         alert(boardList.length);
         localStorage.setItem("list", JSON.stringify(boardList));
-        create_division(board.title);
+        create_division(board);
     });
 
     // FUNC. CREATE DIV
     var create_division = function (item) {
-        $(".row").append("<div class='col-sm-3'><div class='col-sm-12'style='height: 300px; border-radius: 10px; background: #FFE345'><h1></h1></div></div>");
-        $("h1:last").html(item);
+        $(".divBoard").append("<div class='col-sm-3'><a href='/cards'><div class='col-sm-12 board'><h1></h1><h2></h2></div></a></div>");
+        $("h1:last").html(item.title);
     }
 
+    // DETAILED PAGE
+    function getBoardTitle() {
+        $("div.board").click(function () {
+            var title = $(this).find("h1");
+            var innerTitle = title[0].innerHTML;
+            localStorage.setItem("boardTitle", innerTitle)
+        });
+    };
+    function setDetailedTitle() {
+        var innerTitle = localStorage.getItem("boardTitle")
+        $(".divBoardHeader").append("<h1></h1>")
+        $(".divBoardHeader h1:first").html(innerTitle);
+    };
     // START
     var board1 = new Board("board1");
     var board2 = new Board("board2");
@@ -40,5 +54,6 @@ $(document).ready(function () {
         list_all_board(boardList);
         localStorage.setItem("list", JSON.stringify(boardList))
     };
-
+    getBoardTitle();
+    setDetailedTitle();
 });
