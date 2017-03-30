3437a98c200d41ebea8bb7a577b68b68569694df
David Szilard <szilard.david91@gmail.com>
2017-03-30 16:21:36 +0200

    heroku version

--- connect_database.py
+++ connect_database.py
@@ -11,4 +11,4 @@ class ConnectDatabase:
             print("You need to create a database and store its name in a file named 'connect_str.txt'. \
                   For more info, head over to the README")
 
-    db = PostgresqlDatabase(get_connect_string())
+    db = PostgresqlDatabase('d4lpcgm00g22ht', user='ezckpelqnqmmda', password='20686cb1a7d6bae8460e3adf71eb817b6523f9ac0308bea4a6dda435c990e73b', host='ec2-54-247-92-185.eu-west-1.compute.amazonaws.com' )
--- init_database.py
+++ init_database.py
@@ -4,5 +4,5 @@ from models import *
 class InitDatabase:
 
     def init_db():
-        ConnectDatabase.db.drop_tables([Board, Card], safe=True)
+        #ConnectDatabase.db.drop_tables([Board, Card], safe=True)
         ConnectDatabase.db.create_tables([Board, Card], safe=True)
